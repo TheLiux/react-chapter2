cd26f11689033e93dac877a6c7fe391631bbbea7
Stefano Liuzzo Scorpo <stefano.liuzzo@devmy.it>
2024-06-06 16:40:27 +0200

    feat: added sidebar component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
-function AppTitle() {
-  return <h1>Todo app</h1>;
-}
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 const defaultImage = "https://via.placeholder.com/32x32.png";
 
-function getImageOrDefaultURL(imageUrl) {
-  try {
-    new URL(imageUrl);
-    return imageUrl;
-  } catch {
-    return defaultImage;
-  }
-}
-
-function AppMessage({ user: { id, name, image } }) {
-  return (
-    <p>
-      Benvenuto {name} ({id})!
-      <img src={getImageOrDefaultURL(image)} width="32" height="32" />
-    </p>
-  );
-}
 
 const user = {
-  id: 1,
-  name: "Stefano",
-  image: "https://github.com/TheLiux.png",
+    id: 1,
+    name: "Stefano",
+    image: "https://github.com/TheLiux.png",
 };
 
 export default function App() {
-  return (
-    <div>
-      <AppTitle />
-      <AppMessage user={user} />
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <Sidebar user={user}/>
+                <Main/>
+            </div>
+        </div>
+    );
 }
